c4b447b7c07bdb0916be956d4a606e6dbbf21afc
Austin Harrison <aharrison13@ewu.edu>
2023-04-06 16:40:44 -0700

    Deleted an unnecessary "main" file

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import cscd212classes.lab2.Television;
-
-public class Main {
-    public static void main(String[] args) {
-        Television tv1 = new Television("samsung", "sd11", true, 55, 2160 );
-        System.out.print(tv1.toString());
-    }
-}
